e0185c82bbcdc8fd4352ba2338027af9ee586900
Marc Pechaitis <marcpechaitis@gmail.com>
2016-08-11 08:12:21 -0600

    add more json data files

---
+++ data/fruit.php
@@ -0,0 +1,46 @@
+[
+  {
+    "title": "Apples",
+    "price": 1.25
+  },
+  {
+    "title": "Grapes",
+    "price": 1.5
+  },
+  {
+    "title": "Oranges",
+    "price": 2.5
+  },
+  {
+    "title": "Bananas",
+    "price": 1.5
+  },
+  {
+    "title": "Pears",
+    "price": 1.4
+  },
+  {
+    "title": "Kiwis",
+    "price": 1
+  },
+  {
+    "title": "Kiwis",
+    "price": 1
+  },
+  {
+    "title": "Kiwis",
+    "price": 1
+  },
+  {
+    "title": "Kiwis",
+    "price": 1
+  },
+  {
+    "title": "Kiwis",
+    "price": 1
+  },
+  {
+    "title": "Kiwis",
+    "price": 1
+  }
+]
